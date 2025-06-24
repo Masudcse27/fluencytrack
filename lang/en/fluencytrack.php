5492d1c70787e4d6603c861c3ceafc5c789bf812
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-06-24 19:36:58 +0600

    Redirect teachers from view page to teacher dashboard
    - Detect if user has teacher role
    - Automatically redirect to teacher dashboard page

--- lang/en/fluencytrack.php
+++ lang/en/fluencytrack.php
@@ -2,4 +2,5 @@
 
 $string['pluginname'] = 'Fluency Track';
 $string['modulename'] = 'Fluency Track';
+$string['audioupload'] = 'Upload your audio file';
 $string['fluencytrack:viewdashboard'] = 'View Teacher Dashboard';
--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -10,6 +10,10 @@ $context = context_module::instance($cm->id);
 
 require_login($cm->course, true, $cm);
 
+if (!has_capability('mod/fluencytrack:viewdashboard', $context)) {
+    redirect(new moodle_url('/mod/fluencytrack/view.php', ['id' => $id]));
+}
+
 $PAGE->set_url('/mod/fluencytrack/teacher_dashboard.php', ['id' => $id]);
 $PAGE->set_context($context);
 $PAGE->set_title("Teacher Dashboard - {$instance->name}");

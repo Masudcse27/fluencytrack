19fda8cee3605b2e4a0cd52607fc952ffbb9451d
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-06-24 18:18:42 +0600

    Initialize Fluency Track mod plugin project
    - Created `fluencytrack` database table
    - Created `fluencytrack_submissions` database table

---
+++ lang/en/fluencytrack.php
@@ -0,0 +1,4 @@
+<?php
+
+$string['pluginname'] = 'Fluency Track';
+$string['modulename'] = 'Fluency Track';
---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_fluencytrack';
+$plugin->version = 2025061900;
+$plugin->requires = 2022041900; 
+$plugin->cron = 0;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v1.0';
